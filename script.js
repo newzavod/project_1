1103f24ca2a028dcd78b224dc582d35ec2593e74
Vladimir <newzavod@gmail.com>
2021-11-16 20:39:29 +0300

    add cycle, add count

--- script.js
+++ script.js
@@ -9,12 +9,35 @@ const personalMoviesDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько Вы его оцените?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько Вы его оцените?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько Вы его оцените?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько Вы его оцените?', '');
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+// personalMoviesDB.movies[a] = b;
+// personalMoviesDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько Вы его оцените?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMoviesDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMoviesDB.count < 10) {
+    console.log('просмотрено мало фильмов');
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log('Вы классный зритель');
+} else if (personalMoviesDB.count >= 30) {
+    console.log('вы киноман');
+} else {
+    console.log('произошла ошибка');
+}
 
 console.log(personalMoviesDB);
